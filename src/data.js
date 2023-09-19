d46afc60bd0cc4fd4841ead92b0ca874c6f62cde
Mohit Bhandari <mohitbhandari6112@gmail.com>
2023-09-19 18:56:13 +0545

    cart practice

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    id: 1,
+    title: "Samsung Galaxy S12",
+    price: 599.99,
+    img: "./images/image1.jpeg",
+    amount: 1,
+  },
+  {
+    id: 2,
+    title: "google pixel 7",
+    price: 499.99,
+    img: "./images/image2.jpeg",
+    amount: 1,
+  },
+  {
+    id: 3,
+    title: "Xiaomi Redmi Note 12",
+    price: 699.99,
+    img: "./images/image3.jpeg",
+    amount: 1,
+  },
+];
